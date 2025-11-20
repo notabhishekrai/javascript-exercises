68cde5a9102a8482bc06f63bbae3744d9f0b5d95
Abhishek Rai <abhishekraikhaling@gmail.com>
2025-11-20 13:17:13 +0545

    Wrote Palindrome function

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,5 +1,7 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    let sep_string = string.replace(/[^a-zA-Z0-9]/g, "").toLowerCase();
+    let reverse_string = sep_string.split('').reverse().join("");
+    return reverse_string === sep_string;
 };
 
 // Do not edit below this line
