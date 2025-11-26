cb28f909c2f9ba7df9cc9b29bef5c2077ac7b6b6
Abhishek Rai <abhishekraikhaling@gmail.com>
2025-11-27 00:14:13 +0545

    Finish findTheOldest

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,5 +1,13 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    people.forEach(person => {
+        if(!person['yearOfDeath']){
+            person['yearOfDeath'] = new Date().getFullYear();
+        }
+    })
+    const age = person => person.yearOfDeath - person.yearOfBirth;
+    return people.reduce((oldestPerson, person)=>{
+      return age(person) < age(oldestPerson)? oldestPerson:person;        
+    })
 };
 
 // Do not edit below this line
