de004d26658d50aa19632abad60ad35e38f9bf61
Abhishek Rai <abhishekraikhaling@gmail.com>
2025-11-26 12:05:43 +0545

    Finish fibonacci

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,6 +1,10 @@
-const fibonacci = function() {
-
+const fibonacci = function(position) {
+    if(position < 0) {return 'OOPS'} 
+    let fib_seq = [0,1];
+    for(let i = 2; i <= 50; i++){
+      fib_seq.push(fib_seq[(i-1)] + fib_seq[(i-2)]);
+    }
+    return fib_seq[position];
 };
-
 // Do not edit below this line
 module.exports = fibonacci;
