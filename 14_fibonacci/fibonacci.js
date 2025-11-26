c439e1fcdbaf19a1c3823fab3acba5ac7292d39f
Abhishek Rai <abhishekraikhaling@gmail.com>
2025-11-26 12:09:44 +0545

    Change for loop to iterate upto necessary position, change push to simple adding

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,8 +1,8 @@
 const fibonacci = function(position) {
     if(position < 0) {return 'OOPS'} 
     let fib_seq = [0,1];
-    for(let i = 2; i <= 50; i++){
-      fib_seq.push(fib_seq[(i-1)] + fib_seq[(i-2)]);
+    for(let i = 2; i <= position; i++){
+      fib_seq[i] = fib_seq[(i-1)] + fib_seq[(i-2)];
     }
     return fib_seq[position];
 };
